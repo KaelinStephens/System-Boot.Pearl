da4457cd20daea2031025c016371a2e90ca48a6e
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-09 09:53:14 -0600

    added pre startup sequence

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please state your name.");
+            Console.WriteLine("Please identify yourself.");
             var name = Console.ReadLine();
+            Console.WriteLine($"Greetings, {name}. Please state preferred customization options.");
+            var setting = int.Parse(Console.ReadLine());
+            Console.WriteLine("Default setting selected. Please stand by.");
+            Console.WriteLine("*whimsical music plays*");
+            Console.WriteLine("...");
+            Console.WriteLine("*adorable figure appears*")
 
 
             Console.WriteLine($"How do you do, my {name}? Thank you for bringing me into the world. I am at your eternal service. Welcome to your new Pearl.");
