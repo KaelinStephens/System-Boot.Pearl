7a0233dfd43b468a4ff9e50d6162a968284c9b93
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-09 10:02:00 -0600

    added pre startup sequence

--- Program.cs
+++ Program.cs
@@ -9,14 +9,8 @@ namespace HelloWorld
             Console.WriteLine("Please identify yourself.");
             var name = Console.ReadLine();
             Console.WriteLine($"Greetings, {name}. Please state preferred customization options.");
-            var setting = int.Parse(Console.ReadLine());
-            Console.WriteLine("Default setting selected. Please stand by.");
-            Console.WriteLine("*whimsical music plays*");
-            Console.WriteLine("...");
-            Console.WriteLine("*adorable figure appears*")
-
-
-            Console.WriteLine($"How do you do, my {name}? Thank you for bringing me into the world. I am at your eternal service. Welcome to your new Pearl.");
+            var setting = Console.ReadLine();
+            Console.WriteLine($"{setting} = Default setting selected. Please stand by...*whimsical music plays* .....*adorable figure appears* How do you do, my {name}? Thank you for bringing me into the world. I am at your eternal service. Welcome to your new Pearl.");
 
            
         }
