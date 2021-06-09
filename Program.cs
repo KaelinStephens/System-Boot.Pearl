8f3b3d8d71b3633322f05485ea95228fcf9cf957
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-09 09:37:13 -0600

    initial commit for Pearl interface

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please state your name.");
+            var name = Console.ReadLine();
+
+
+            Console.WriteLine($"How do you do, my {name}? Thank you for bringing me into the world. I am at your eternal service. Welcome to your new Pearl.");
+
+           
+        }
+    }
+}
